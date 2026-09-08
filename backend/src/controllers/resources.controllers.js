847a25539693e5fbf9418a0551913c16ac0708d3
Dhruv-dev-89 <dhruvimsec10@gmail.com>
2026-09-08 22:52:08 +0530

    written resource api's

--- backend/src/controllers/resources.controllers.js
+++ backend/src/controllers/resources.controllers.js
@@ -50,4 +50,56 @@ const getResourceById=async (req,res)=>{
         })
     }
 }
-export {createResources,getResourceById}
+
+const getResource=async (req,res)=>{
+    try {
+        const resources=await prisma.resource.findMany();
+
+        if(resources.length===0){
+            res.status(404).json({
+                message:"Resources are not found for the organization"
+            })
+        }
+
+        res.status(200).json({resources});
+    } catch (error) {
+        res.status(500).json({
+            message:"Failed to fetch resources",
+            error:error.message
+        })
+    }
+}
+
+const updateResource=async (req,res)=>{
+    try {
+        const resourceId=req.params.id;
+        const updatedName=req.body.name;
+        const updatedDescription=req.body.description;
+        const organizationId=req.body.organizationId;
+
+        const updatedResource=await prisma.resource.update({
+            where:{
+                id:Number(resourceId)
+            },
+            data:{
+                ...(updatedName!==undefined&&{name:updatedName}),
+                ...(updatedDescription!==undefined&&{description:updatedDescription}),
+                ...(organizationId!==undefined&&{organizationId:organizationId})
+            }
+        })
+
+        res.status(200).json({updatedResource})
+    } catch (error) {
+        if(error.code==="P2025"){
+            return res.status(404).json({
+                message:"resource not found"
+            })
+        }
+        
+        res.status(500).json({
+            message:"Failed to Update resource",
+            error:error.message
+        })
+    }
+}
+export {createResources,getResourceById,getResource,updateResource}
--- backend/src/routes/resources.routes.js
+++ backend/src/routes/resources.routes.js
@@ -1,10 +1,14 @@
 import express from "express"
-import { createResources, getResourceById} from "../controllers/resources.controllers.js";
+import { createResources, getResource, getResourceById, updateResource} from "../controllers/resources.controllers.js";
 
 const router=express.Router();
 
 router.post("/resource",createResources);
 
 router.get("/resource/:id",getResourceById);
+
+router.get("/resources",getResource);
+
+router.patch("/update-resource/:id",updateResource)
 
 export default router
